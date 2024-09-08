eaa248f032866d7ec5803685bc4e0f998df03e9d
Tiago Passinato <tiagopassinato@gmail.com>
2024-09-08 08:26:27 -0400

    hardcode backend url

--- src/api/config/servicePort.ts
+++ src/api/config/servicePort.ts
@@ -1,5 +1,5 @@
 // 后端微服务端口名
 export const PORT1 = "/hooks";
 export const PORT2 = "/geeker";
-export const PORT3 = "http://ng8o8ooocws8gg0cogo00084.47.253.146.52.sslip.io";
+export const PORT3 = "http://api-staging.voyapay.com";
 // export const PORT3 = "http://localhost:3333";
