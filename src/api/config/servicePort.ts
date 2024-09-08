05c16dba6c601720abcaf5ed61278f2a6a2c4015
yifan <ywang289@buffalo.edu>
2024-09-08 10:14:19 -0400

    add backend url to env

--- src/api/config/servicePort.ts
+++ src/api/config/servicePort.ts
@@ -1,5 +1,8 @@
 // 后端微服务端口名
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const PORT1 = "/hooks";
 export const PORT2 = "/geeker";
-export const PORT3 = "http://api-staging.voyapay.com";
+export const PORT3 = process.env.backend_urls;
 // export const PORT3 = "http://localhost:3333";
