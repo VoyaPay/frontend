7ce948a4c4033b481d2ce709be9415a6e550bc88
yifan <ywang289@buffalo.edu>
2024-09-23 09:55:44 -0400

    change error

--- src/components/ErrorMessage/403.tsx
+++ src/components/ErrorMessage/403.tsx
@@ -11,8 +11,8 @@ const NotAuth = () => {
 	return (
 		<Result
 			// status="403"
-			// title="403"
-			subTitle="Welcome To Voyapay"
+			title="Welcome to Voypay"
+			
 			extra={
 				<Button type="primary" onClick={goHome}>
 					Back Home
