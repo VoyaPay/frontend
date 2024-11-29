c47eabe36d4b2df7d908b558a6f8d95ef59a71c2
Yang.he <yang@voyapay.com>
2024-11-29 09:36:48 +0800

    feat: global add http 401 back login

--- src/api/index.ts
+++ src/api/index.ts
@@ -51,36 +51,29 @@ class RequestHttp {
 		 *  服务器换返回信息 -> [拦截统一处理] -> 客户端JS获取到信息
 		 */
 		this.service.interceptors.response.use(
+
 			(response: AxiosResponse) => {
 				const { data, config } = response;
 				NProgress.done();
-				// * 在请求结束后，移除本次请求(关闭loading)
 				axiosCanceler.removePending(config);
 				tryHideFullScreenLoading();
-				// * 登录失效（code == 599）
-				if (data.code == ResultEnum.OVERDUE) {
-					store.dispatch(setToken(""));
-					message.error(data.msg);
-					window.location.hash = "/login";
-					return Promise.reject(data);
-				}
-				// * 全局错误信息拦截（防止下载文件得时候返回数据流，没有code，直接报错）
 				if (data.code && data.code !== ResultEnum.SUCCESS) {
 					message.error(data.msg);
 					return Promise.reject(data);
 				}
-				// * 成功请求（在页面上除非特殊情况，否则不用处理失败逻辑）
 				return data;
 			},
 			async (error: AxiosError) => {
 				const { response } = error;
 				NProgress.done();
 				tryHideFullScreenLoading();
-				// 请求超时单独判断，请求超时没有 response
-				if (error.message.indexOf("timeout") !== -1) message.error("请求超时，请稍后再试");
-				// 根据响应的错误状态码，做不同的处理
+				if (response?.status === 401) {
+					store.dispatch(setToken(""));
+					message.error('您的会话已过期，请重新登录。');
+					window.location.hash = "/login";
+					return Promise.reject(error);
+				}
 				if (response) checkStatus(response.status);
-				// 服务器结果都没有返回(可能服务器错误可能客户端断网) 断网处理:可以跳转到断网页面
 				if (!window.navigator.onLine) window.location.hash = "/500";
 				return Promise.reject(error);
 			}
