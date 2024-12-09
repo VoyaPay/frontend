acb2c1f5ec84a39b226e1937b0edb07e17222f2b
Yang.he <yang@voyapay.com>
2024-12-09 11:15:02 +0800

    feat(VMT-489): add mobile logic

--- src/hooks/useInactivityLogout.ts
+++ src/hooks/useInactivityLogout.ts
@@ -12,11 +12,13 @@ export default function useInactivityLogout(timeout = 20 * 60 * 1000) {
 			timer = setTimeout(() => setIsActive(false), timeout);
 		};
 		window.addEventListener("mousemove", handleActivity);
+		window.addEventListener("touchstart", handleActivity);
 		timer = setTimeout(() => setIsActive(false), timeout);
 
 		return () => {
 			clearTimeout(timer);
 			window.removeEventListener("mousemove", handleActivity);
+			window.removeEventListener("touchstart", handleActivity);
 		};
 	}, [timeout, isActive]);
 
