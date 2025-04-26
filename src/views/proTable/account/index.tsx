212ca4d05cc94ed2365363f7870234b77b4d7536
cho <zhqchen@126.com>
2025-04-26 21:37:44 +0800

    fix: build error

--- src/views/proTable/account/index.tsx
+++ src/views/proTable/account/index.tsx
@@ -63,7 +63,7 @@ const Account: React.FC = () => {
 	const getTransactionStatistics = async (startDate?: string, endDate?: string) => {
 		try {
 			const response = await TransactionStatisticApi({ startDate, endDate });
-			setTransactionData(response);
+			setTransactionData(response.data as TransactionData);
 		} catch (error) {
 			console.error("获取交易统计数据失败:", error);
 		}
